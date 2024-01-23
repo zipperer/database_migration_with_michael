a1239add69aac6013316c72460fb935250b02930
zipperer <zipperer.andrew@gmail.com>
2024-01-23 13:57:07 -0600

    in emacs, set-buffer-file-coding-system to utf8-unix and rerun space_separated_to_pipe_separated.py

--- title_program_numbers_and_bible_verse_timestamp_files/title_program_numbers_into_db/space_separated_to_pipe_separated.py
+++ title_program_numbers_and_bible_verse_timestamp_files/title_program_numbers_into_db/space_separated_to_pipe_separated.py
@@ -3,7 +3,7 @@ import csv
 filename_file_to_read = 'program_number_and_title.csv'
 with open(filename_file_to_read) as file_object_file_to_read:
     file_lines_file_to_read = file_object_file_to_read.readlines()
-filename_file_to_write = 'program_number_and_title_pipe_separated_quote_none.csv'
+filename_file_to_write = 'program_number_and_title_pipe_separated.csv'
 fieldnames_for_input_file=['program_id', 'program_code', 'program_title']
 with open(filename_file_to_write, 'w', newline='') as file_object_file_to_write:
     csv_writer = csv.DictWriter(file_object_file_to_write, fieldnames=fieldnames_for_input_file, delimiter='|')
