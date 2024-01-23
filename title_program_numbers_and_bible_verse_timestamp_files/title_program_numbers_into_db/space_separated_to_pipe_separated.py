19dbf6116e4abfb97d5fc74d27fb5e117d4a8b65
zipperer <zipperer.andrew@gmail.com>
2024-01-23 11:51:22 -0600

    remove experiment with quoting=csv.QUOTE_NONE and escapechar="\"

--- title_program_numbers_and_bible_verse_timestamp_files/title_program_numbers_into_db/space_separated_to_pipe_separated.py
+++ title_program_numbers_and_bible_verse_timestamp_files/title_program_numbers_into_db/space_separated_to_pipe_separated.py
@@ -6,7 +6,7 @@ with open(filename_file_to_read) as file_object_file_to_read:
 filename_file_to_write = 'program_number_and_title_pipe_separated_quote_none.csv'
 fieldnames_for_input_file=['program_id', 'program_code', 'program_title']
 with open(filename_file_to_write, 'w', newline='') as file_object_file_to_write:
-    csv_writer = csv.DictWriter(file_object_file_to_write, fieldnames=fieldnames_for_input_file, delimiter='|', escapechar='\\', quoting=csv.QUOTE_NONE)
+    csv_writer = csv.DictWriter(file_object_file_to_write, fieldnames=fieldnames_for_input_file, delimiter='|')
     csv_writer.writeheader()
     for file_line in file_lines_file_to_read:
         file_line_split_into_three_fields = file_line.split(sep=' ', maxsplit=2)
